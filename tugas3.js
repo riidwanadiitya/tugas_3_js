d8e304ab73a5a2daee8cda851a6e4cdcc549ecc8
Ridwan <71011389+riidwanadiitya@users.noreply.github.com>
2021-01-04 08:50:51 +0700

    TUGAS 3 JS

---
+++ tugas3.js
@@ -0,0 +1,8 @@
+function namaSiswa(x = "Ridwan", y = "Aditya") {
+    var namaDepan = x;
+    var namaBelakang = y;
+    var namaLengkap = namaDepan + " " + namaBelakang;
+    return console.log(namaLengkap);
+}
+namaSiswa("andika", "rizky");
+namaSiswa();
